97b054a89fdee79ec60f1567a15adf51b97625bb
i-guerrero <isaacguerrero@pursuit.org>
2023-04-22 16:33:23 -0400

    Add data and db directories

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS greenrunners_dev;
+CREATE DATABASE greenrunners_dev;
+
+\c greenrunners_dev;
+
+CREATE TABLE greenscores (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    electric INT,
+    gas INT,
+    oil INT,
+    car_mileage INT,
+    flights INT,
+    recycle_newspaper BOOLEAN,
+    recycle_aluminum BOOLEAN
+);
